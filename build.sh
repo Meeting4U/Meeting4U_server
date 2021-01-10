2d57fb38b24234fd68be1bd01515a5d5f8838ea7
siksik92 <jang8476803@naver.com>
2021-01-10 18:23:37 +0900

    add : build및 실행 readme 추가, dev 환경 build script 추가

---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cd meeting4u/server/Meeting4U_server
+
+# git 변경내용 fetch 및 merge
+git fetch
+
+h1=`git rev-parse HEAD`
+h2=`git rev-parse origin/main`
+
+if [[ $h1 == $h2 ]]; then
+    echo "Ok"
+    exit 0
+fi
+
+echo "now build.."
+
+git merge origin/main
+
+# 현재 기동중인 서버 중지.
+pid=`jps | grep Gradle | awk '{print $1}'`
+kill $pid
+
+# 프로젝트 rebuild
+./gradlew clean build
+
+#서버 실행.
+cd build/libs
+java -jar -Dspring.profiles.active=dev Meeting4U_server-1.0-SNAPSHOT.jar
+
